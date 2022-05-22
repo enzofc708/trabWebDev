1096b1a4b26d193d01f7c672230e471c0b71de1e
enzofc708 <47937189+enzofc708@users.noreply.github.com>
2022-05-22 11:23:53 -0300

    finalmente algum progresso meu deus do ceu

--- square_progression.js
+++ square_progression.js
@@ -4,33 +4,40 @@ let sliderValue = document.getElementById("ratioValue");
 let NValue = document.getElementById("NValue");
 sliderValue.innerHTML = slider.value;
 NValue.innerHTML = Nslider.value;
-let desenho = document.getElementById("desenho");
+let desenhoDiv = d3.select("#divDesenho");
 
 let update = () => {
-    desenho.innerHTML = "";
-    let finalSvg;
+    desenhoDiv.html("");
     const colors = ["red", "yellow", "blue", "green"];
     const angle = +slider.value * Math.PI / 2;
+    let count = 1;
+    desenhoDiv.append("svg")
+           .attr("id", "desenho")
+           .attr("width", 800)
+           .attr("height", 600)
+           .attr("style", "border:1px solid gray");
+
+    let desenho = d3.select("#desenho");
+
+    desenho.append("g")
+           .attr("id", "g1")
+           .attr("transform", "translate(400, 300)");
     for (let i = 0; i < +Nslider.value; i++) {
-        let g = document.createElement("g");
-        g.setAttribute("transform", `translate(80,0) rotate(${angle}) scale(${1/(Math.acos(angle))})`);        
+        let gAtualId = `#g${count++}`;
+        let gAtual = d3.select(gAtualId);
+
+        gAtual.append("rect")
+              .attr("fill", colors[i % colors.length])
+              .attr("x", "0")
+              .attr("y", "0")
+              .attr("width", "80")
+              .attr("height", "80")
+              .attr("opacity", "0.7");
         
-        let rect = document.createElement("rect");
-        rect.setAttribute("fill", colors[i % colors.length]);
-        rect.setAttribute("x", "0");
-        rect.setAttribute("y", "0");
-        rect.setAttribute("width", "80");
-        rect.setAttribute("height", "80");
-        rect.setAttribute("opacity", "0.7");
-        
-        g.appendChild(rect);
-        finalSvg == null ? finalSvg : g.appendChild(finalSvg);
-        finalSvg = g;
+        gAtual.append("g")
+              .attr("id", `g${count}`)
+              .attr("transform", `rotate(${90 * +slider.value}) scale(${1/(Math.acos(angle))})`);
     }
-    let finalG = document.createElement("g");
-    finalG.setAttribute("transform", "translate(400, 300)");
-    finalG.appendChild(finalSvg);
-    desenho.appendChild(finalG);
 };
 
 update();
